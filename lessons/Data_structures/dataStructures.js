cf281c39545ffcec1d031bde47b363d6b4445a83
Cybergman-droid <jasongakpo20@gmail.com>
2026-01-29 21:27:15 +0000

    Add Queue class and refactor Stack implementation
    Introduced a new Queue class with enqueue, dequeue, peek, and show methods. Refactored the Stack class for improved clarity and consistency. Updated example usage to demonstrate both Stack and Queue functionalities.

--- lessons/Data_structures/dataStructures.js
+++ lessons/Data_structures/dataStructures.js
@@ -1,39 +1,89 @@
-class Stack{
-    #array
-    #topPointer
-    constructor(){
-        this.#array = []
-        this.#topPointer = -1
-    }
+class Stack {
+  #items;
+  #topPointer;
+  constructor() {
+    this.#items = [];
+    this.#topPointer = -1;
+  }
 
-    push(element){
-        this.#array.push(element)
-        this.#topPointer += 1
-    }
+  push(item) {
+    this.#items.push(item);
+    this.#topPointer += 1;
+  }
 
-    pop(){
-        this.#topPointer < 0 ?  console.log('The stack is empty') : this.#topPointer -= 1
+  pop() {
+    if (this.#topPointer < 0) {
+      console.log("the stack is empty");
+    } else {
+      this.#topPointer -= 1;
     }
+  }
 
-    peek(){
-        this.#topPointer < 0 ? console.log('The stack is empty') : console.log(this.#array[this.#topPointer])
+  peek() {
+    if (this.#topPointer < 0) {
+      console.log("the stack is empty");
+    } else {
+      console.log(this.#items[this.#topPointer]);
     }
+  }
 
-    show(){
-        console.log(this.#array)
-    }
+  show() {
+    console.log(this.#items);
+  }
 }
 
-let myStack = new Stack()
-myStack.push('15')
-myStack.push('45')
-myStack.push('75')
-myStack.push('95')
-myStack.show()
-myStack.peek()
-myStack.pop()
-myStack.pop()
-myStack.pop()
-myStack.pop()
-myStack.pop()
-myStack.peek()
+class Queue {
+  #items;
+  #frontPointer;
+  #backPointer;
+  constructor() {
+    this.#items = [];
+    this.#frontPointer = -1;
+    this.#backPointer = -1;
+  }
+
+  enqueue(item) {
+    if (!this.#items.length >= 1) {
+      this.#items.push(item);
+      this.#frontPointer += 1;
+      this.#backPointer += 1;
+    } else {
+      this.#items.push(item);
+      this.#backPointer += 1;
+    }
+  }
+
+  dequeue() {
+    if (this.#items.length < 0) {
+      console.log("the queue is empty");
+    } else {
+      this.#frontPointer += 1;
+    }
+  }
+
+  peek() {
+    if (!this.#items.length >= 1) {
+      console.log(this.#items[this.#frontPointer]);
+    }
+  }
+
+  show() {
+    console.log(this.#items);
+  }
+}
+
+let myStack = new Stack();
+let myQueue = new Queue();
+myQueue.enqueue("15");
+myQueue.enqueue("45");
+myQueue.enqueue("75");
+myQueue.enqueue("95");
+myQueue.show();
+myQueue.peek();
+myQueue.dequeue();
+myQueue.dequeue();
+myQueue.dequeue();
+myQueue.dequeue();
+myQueue.dequeue();
+myQueue.show();
+myQueue.peek();
