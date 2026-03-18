a341c23f3efbf33ba038445a3095bc55b0b01920
Cybergman-droid <jasongakpo20@gmail.com>
2026-03-18 10:03:08 +0000

    Add Protect The Castle game with initial setup and assets

---
+++ lessons/games/protect_the_castle/castle.js
@@ -0,0 +1,95 @@
+let state = 0;
+function preload() {}
+
+function setup() {
+  createCanvas();
+  castle = new Sprite();
+  castle.w = 100;
+  castle.h = 100;
+  // castle .img  = castlrImg
+  castle.physics = STATIC;
+  castle.health = 100;
+
+  enemies = new Group();
+  enemies.physics = DYNAMIC;
+
+  cursor = new Sprite();
+  cursor.physics = NONE;
+
+  setInterval(spawnEnemies, 500);
+
+  cursor.overlapping(enemies, mouseAttack);
+  enemies.overlapping(castle, enemyAttack);
+}
+
+function update() {
+  switch (state) {
+    case 0:
+      mouseMovement();
+      enemyMovement(); // playing
+      break;
+    case 1:
+      break;
+  }
+
+  mouseAttack();
+
+  enemyAttack();
+}
+
+function draw() {
+  switch (state) {
+    case 0: // playing
+      background(0);
+      break;
+    case 1: // paused
+      //TODO draw paused menu
+      break;
+  }
+}
+
+function mouseMovement() {
+  cursor.x = mouse.x;
+  cursor.y = mouse.y;
+}
+function mouseAttack(c, e) {
+  if (mouse.released()) {
+    e.remove();
+  }
+}
+function enemyMovement() {
+  for (e of enemies) {
+    e.rotation = e.angleTo(castle);
+    e.direction = e.rotation;
+    e.speed = 1;
+  }
+}
+function enemyAttack(e, c) {
+  c.health -= 0.1;
+}
+
+function spawnEnemies() {
+  let margin = 64;
+  let x, y;
+  if (state == 0) {
+    console.log("WIrk");
+    //spawn logic
+    let side = floor(random(0, 4));
+
+    if (side === 0) {
+      x = random(0, width);
+      y = -margin;
+    } else if (side === 1) {
+      x = random(0, width);
+      y = height + margin;
+    } else if (side === 2) {
+      x = -margin;
+      y = random(0, height);
+    } else {
+      x = width + margin;
+      y = random(0, height);
+    }
+
+    new enemies.Sprite(x, y);
+  }
+}
